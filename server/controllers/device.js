59c9413ca0cb742c5ea6e893d3c6341b4e2fcf96
PetruPractice <petru.duhlicher@mail.ru>
2021-07-05 22:55:05 +0300

    added fundamentals for pagination and ended basic controllers logic

--- server/controllers/device.js
+++ server/controllers/device.js
@@ -1,19 +1,28 @@
 const uuid = require('uuid')
 const path = require('path')
-const { Device } = require('../models/models')
+const { Device, DeviceInfo } = require('../models/models')
 const ApiError = require('../error/apiError')
 
 class deviceController {
     async create(req, res, next) {
         try {
 
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const{img} = req.files
             let filename = uuid.v4() + ".jpg"   // setup the unique file name + woking with file uploading
             img.mv(path.resolve(__dirname, '..', 'static', fileName))//  move file to static folder with path extention from nodejs 
-    
+
             const device = await Device.create({name, price, brandId, typeId, img: filename})
-    
+            
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach( i=>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    }))
+            }
             return res.json(device)
         } catch(e) {
             next(ApiError.badRequest(e.message))
@@ -22,10 +31,34 @@ class deviceController {
     }
     
     async getAll(req, res) {
-
+        const {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page* limit - limit
+        let devices
+        if(!brandId && !typeId){
+            devices = await Device.findAndCountAll(limit, offset)
+        }
+        if(brandId && !typeId){
+            devices = await Device.findAndCountAll({where:{brandId, limit, offset}}) //findAndCountAll() function for pagination
+        }
+        if(!brandId && typeId){
+            devices = await Device.findAndCountAll({where:{typeId, limit, offset}})
+        }
+        if(brandId && typeId){
+            devices = await Device.findAndCountAll({where:{brandId, typeId, limit, offset}})
+        }
+        return res.json(devices)
     }
     async getOne(req, res) {
-
+        const {id} = req.params
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            }
+        )
+        return res.json(device)
     }
 
 }
